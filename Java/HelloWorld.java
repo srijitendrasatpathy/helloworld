347cbeed1bf6b12a044b3c5d1d3efa767d6b6493
srijitendrasatpathy <jitendra.it253@gmail.com>
2018-05-16 07:11:20 -0400

    Added Scanner function to the java file

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 
 public class HelloWorld{
 	public static void main(String args[]){
-		System.out.println("Hello World!\n");
+	Scanner sc = new Scanner(System.in);	
+	System.out.println("Hello World!\n");
+	String name = sc.nextLine();
 	}
 }
